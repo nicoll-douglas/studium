da4c17e1935674798c9f65e2d64e8216ccbb1e83
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-05-07 22:14:24 +0100

    refactor menuitem logic

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,86 +1,50 @@
-export default function MenuItem({ variant }) {
+import BookmarksIcon from "./icons/bookmarks-icon";
+import CalculatorIcon from "./icons/calculator-icon";
+import DashboardIcon from "./icons/dashboard-icon";
+import DictionaryIcon from "./icons/dictionary-icon";
+import NotesIcon from "./icons/notes-icon";
+import PomodoroTimerIcon from "./icons/pomodoro-timer-icon";
+import ToDoListIcon from "./icons/to-do-list-icon";
+import { NavLink } from "react-router-dom";
+
+export default function MenuItem({ variant, role }) {
+  let SVGComponent;
+
+  switch (variant) {
+    case "Bookmarks":
+      SVGComponent = BookmarksIcon;
+      break;
+    case "Notes":
+      SVGComponent = NotesIcon;
+      break;
+    case "Dictionary":
+      SVGComponent = DictionaryIcon;
+      break;
+    case "Pomodoro Timer":
+      SVGComponent = PomodoroTimerIcon;
+      break;
+    case "Calculator":
+      SVGComponent = CalculatorIcon;
+      break;
+    case "To Do List":
+      SVGComponent = ToDoListIcon;
+      break;
+    default:
+      SVGComponent = DashboardIcon;
+  }
+
   return (
-    <li role="menuitem">
-      <a className="group flex gap-2 items-center w-full py-4 cursor-pointer">
-        {variant === "Dashboard" && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 -960 960 960"
-            width="24"
-            className="group-hover:fill-LM-accent-light dark:group-hover:fill-DM-accent-light"
-          >
-            <path d="M666-440 440-666l226-226 226 226-226 226Zm-546-80v-320h320v320H120Zm400 400v-320h320v320H520Zm-400 0v-320h320v320H120Zm80-480h160v-160H200v160Zm467 48 113-113-113-113-113 113 113 113Zm-67 352h160v-160H600v160Zm-400 0h160v-160H200v160Zm160-400Zm194-65ZM360-360Zm240 0Z" />
-          </svg>
-        )}
-        {variant === "To Do List" && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 -960 960 960"
-            width="24"
-            className="group-hover:fill-LM-accent-light dark:group-hover:fill-DM-accent-light"
-          >
-            <path d="M222-200 80-342l56-56 85 85 170-170 56 57-225 226Zm0-320L80-662l56-56 85 85 170-170 56 57-225 226Zm298 240v-80h360v80H520Zm0-320v-80h360v80H520Z" />
-          </svg>
-        )}
-        {variant === "Notes" && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 -960 960 960"
-            width="24"
-            className="group-hover:fill-LM-accent-light dark:group-hover:fill-DM-accent-light"
-          >
-            <path d="M160-120v-170l527-526q12-12 27-18t30-6q16 0 30.5 6t25.5 18l56 56q12 11 18 25.5t6 30.5q0 15-6 30t-18 27L330-120H160Zm80-80h56l393-392-28-29-29-28-392 393v56Zm560-503-57-57 57 57Zm-139 82-29-28 57 57-28-29ZM560-120q74 0 137-37t63-103q0-36-19-62t-51-45l-59 59q23 10 36 22t13 26q0 23-36.5 41.5T560-200q-17 0-28.5 11.5T520-160q0 17 11.5 28.5T560-120ZM183-426l60-60q-20-8-31.5-16.5T200-520q0-12 18-24t76-37q88-38 117-69t29-70q0-55-44-87.5T280-840q-45 0-80.5 16T145-785q-11 13-9 29t15 26q13 11 29 9t27-13q14-14 31-20t42-6q41 0 60.5 12t19.5 28q0 14-17.5 25.5T262-654q-80 35-111 63.5T120-520q0 32 17 54.5t46 39.5Z" />
-          </svg>
-        )}
-        {variant === "Bookmarks" && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 -960 960 960"
-            width="24"
-            className="group-hover:fill-LM-accent-light dark:group-hover:fill-DM-accent-light"
-          >
-            <path d="M120-40v-640q0-33 23.5-56.5T200-760h400q33 0 56.5 23.5T680-680v640L400-160 120-40Zm80-122 200-86 200 86v-518H200v518Zm560 2v-680H240v-80h520q33 0 56.5 23.5T840-840v680h-80ZM200-680h400-400Z" />
-          </svg>
-        )}
-        {variant === "Pomodoro Timer" && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 -960 960 960"
-            width="24"
-            className="group-hover:fill-LM-accent-light dark:group-hover:fill-DM-accent-light"
-          >
-            <path d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z" />
-          </svg>
-        )}
-        {variant === "Calculator" && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 -960 960 960"
-            width="24"
-            className="group-hover:fill-LM-accent-light dark:group-hover:fill-DM-accent-light"
-          >
-            <path d="M300-520q-58 0-99-41t-41-99q0-58 41-99t99-41q58 0 99 41t41 99q0 58-41 99t-99 41Zm0-80q25 0 42.5-17.5T360-660q0-25-17.5-42.5T300-720q-25 0-42.5 17.5T240-660q0 25 17.5 42.5T300-600Zm360 440q-58 0-99-41t-41-99q0-58 41-99t99-41q58 0 99 41t41 99q0 58-41 99t-99 41Zm0-80q25 0 42.5-17.5T720-300q0-25-17.5-42.5T660-360q-25 0-42.5 17.5T600-300q0 25 17.5 42.5T660-240Zm-444 80-56-56 584-584 56 56-584 584Z" />
-          </svg>
-        )}
-        {variant === "Dictionary" && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 -960 960 960"
-            width="24"
-            className="group-hover:fill-LM-accent-light dark:group-hover:fill-DM-accent-light"
-          >
-            <path d="M160-391h45l23-66h104l24 66h44l-97-258h-46l-97 258Zm81-103 38-107h2l38 107h-78Zm319-70v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-600q-38 0-73 9.5T560-564Zm0 220v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-380q-38 0-73 9t-67 27Zm0-110v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-490q-38 0-73 9.5T560-454ZM260-320q47 0 91.5 10.5T440-278v-394q-41-24-87-36t-93-12q-36 0-71.5 7T120-692v396q35-12 69.5-18t70.5-6Zm260 42q44-21 88.5-31.5T700-320q36 0 70.5 6t69.5 18v-396q-33-14-68.5-21t-71.5-7q-47 0-93 12t-87 36v394Zm-40 118q-48-38-104-59t-116-21q-42 0-82.5 11T100-198q-21 11-40.5-1T40-234v-482q0-11 5.5-21T62-752q46-24 96-36t102-12q58 0 113.5 15T480-740q51-30 106.5-45T700-800q52 0 102 12t96 36q11 5 16.5 15t5.5 21v482q0 23-19.5 35t-40.5 1q-37-20-77.5-31T700-240q-60 0-116 21t-104 59ZM280-499Z" />
-          </svg>
-        )}
+    <li role={role}>
+      <NavLink
+        className="group flex gap-2 items-center w-full py-4 cursor-pointer"
+        to={`/${variant
+          .split(" ")
+          .map((word) => word.toLowerCase())
+          .join("-")}`}
+      >
+        <SVGComponent className="group-hover:fill-LM-accent-light dark:group-hover:fill-DM-accent-light" />
         {variant}
-      </a>
+      </NavLink>
     </li>
   );
 }
