fd25492e138c25cdb5874a09aca7284bba2729a1
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-05-07 23:09:11 +0100

    add title change effect

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,30 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import { useEffect } from "react";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   const location = useLocation();
   useEffect(() => {
     const currentTitle = document.title;
-
+    const appPaths = [
+      "/dashboard",
+      "/notes",
+      "/to-do-list",
+      "/calculator",
+      "/dictionary",
+      "/pomodoro-timer",
+      "/bookmarks",
+    ];
     const pathname = location.pathname;
+
     if (pathname === "/" || pathname === "/home") {
       document.title = "Studium | The home of your on-the-fly study needs";
-      return;
+    } else if (appPaths.some((path) => path === pathname)) {
+      document.title = "Studium | App";
+    } else {
+      document.title = "404 Page Not Found";
     }
-
-    document.title = `Studium | ${pathname
-      .slice(1)
-      .split("-")
-      .map((word) => word[0].toUpperCase() + word.slice(1))
-      .join(" ")}`;
 
     return () => {
       document.title = currentTitle;
@@ -37,9 +44,9 @@ function App() {
         <Route path="/calculator"></Route>
         <Route path="/dictionary"></Route>
         <Route path="/pomodoro-timer"></Route>
-        <Route path="/dictionary"></Route>
         <Route path="/bookmarks"></Route>
       </Route>
+      <Route path="*" element={<ErrorPage />}></Route>
     </Routes>
   );
 }
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -7,7 +7,7 @@ export default function ErrorPage() {
       <Header hasNav={false} />
       <div className="flex-grow flex items-center flex-col justify-center py-16">
         <h1 className="text-9xl flex gap-2 items-center">404</h1>
-        <p className="text-3xl mb-5">Page not found.</p>
+        <p className="text-3xl mb-5">Page not found</p>
         <Link
           to="/"
           className="py-3 px-8 font-semibold rounded-xl bg-LM-accent-light text-white dark:text-black text-lg flex gap-2 items-center hover:bg-LM-accent-dark dark:bg-DM-accent-light dark:hover:bg-DM-accent-dark transition-colors duration-200 group max-h-fit self-start mx-auto lg:text-base lg:px-6 md:px-4 hover:text-white dark:hover:text-black"
