9d8c5bfa9a9521554afe4a0de951318d4b782c64
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-05-17 17:02:17 +0100

    rename folder

--- src/pages/Notes/components/NoNotes.jsx
+++ src/pages/Notes/components/NoNotes.jsx
@@ -1,7 +1,7 @@
 export default function NoNotes() {
   return (
     <div className="w-full flex items-center justify-center col-span-4">
-      <div className="border text-center rounded-xl p-6 w-[500px] border-LM-accent-light dark:border-DM-accent-light lg:w-[390px] sm:w-full mx-auto">
+      <div className="border text-center rounded-xl p-6 w-[500px] border-LM-accent-light dark:border-DM-accent-light lg:w-[390px] sm:w-full mx-auto -mt-4">
         Add a note to get started
       </div>
     </div>
