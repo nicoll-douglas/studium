b5f4310633c4ae47db2e7d73236c16927fd3985e
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-05-24 10:00:05 +0100

    refactor titleMap util

--- src/utils/titleMap.js
+++ src/utils/titleMap.js
@@ -1,12 +1,16 @@
 export default function mapToTitle(pathname) {
-  return {
-    "/dashboard": "Dashboard",
-    "/calculator": "Calculator",
-    "/pomodoro-timer": "Pomodoro Timer",
-    "/bookmarks": "Bookmarks",
-    "/to-do-list": "To Do List",
-    "/notes": "Notes",
-    "/home": "The home of your on the fly study needs",
-    "/": "The home of your on the fly study needs",
-  }[pathname];
+  switch (pathname) {
+    case "/dashboard":
+      return "Dashboard";
+    case "/calculator":
+      return "Calculator";
+    case "/pomodoro-timer":
+      return "Pomodoro Timer";
+    case "/bookmarks":
+      return "Bookmarks";
+    case "/to-do-list":
+      return "To Do List";
+    default:
+      return "The home of your on the fly study needs";
+  }
 }
